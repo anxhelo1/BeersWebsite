f3892338a82faee8526e76c7791a074703a8ab8d
Anxhelo <anxhelosako@gmail.com>
2018-04-14 16:36:50 +0200

    adding template and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './components/beer-list/beer-list.component';
 import { BeerDetailsComponent } from './components/beer-details/beer-details.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BeerListComponent,
-    BeerDetailsComponent
+    BeerDetailsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
