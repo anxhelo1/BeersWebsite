ba57ab9bc2c11d5fe237413300518ba813f32246
Anxhelo <anxhelosako@gmail.com>
2018-04-15 15:23:03 +0200

    removing hardcoded apiURl and apiKey

--- src/app/services/beer.service.ts
+++ src/app/services/beer.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/observable';
+import {environment} from 'environments/environment';
 
 @Injectable()
 export class BeerService {
@@ -10,11 +11,11 @@ export class BeerService {
 
   getBeers(search: string = '', order: string = 'name', sort: string = 'ASC'): Observable<any> {
     return this.httpClient
-      .get(`http://api.brewerydb.com/v2/beers?key=ee8a1a84bc76fd7d7ae6dd0dc45583e3&name=${search}&order=${order}&sort=${sort}`);
+      .get(`${environment.apiUrl}beers?key=${environment.apiKey}&name=${search}&order=${order}&sort=${sort}`);
   }
 
   getBeerById(id: string): Observable<any> {
-    return this.httpClient.get(`http://api.brewerydb.com/v2/beer/${id}?key=ee8a1a84bc76fd7d7ae6dd0dc45583e3`);
+    return this.httpClient.get(`${environment.apiUrl}/beer/${id}?key=${environment.apiKey}`);
   }
 
 }
