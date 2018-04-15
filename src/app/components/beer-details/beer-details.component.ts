b681a86e7865febab1b3189a971cd3f5fd009d8e
Anxhelo <anxhelosako@gmail.com>
2018-04-15 15:09:52 +0200

    details component development

--- src/app/components/beer-details/beer-details.component.ts
+++ src/app/components/beer-details/beer-details.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BeerService} from '../../services/beer.service';
+import {Beer} from '../../models/Beer';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-beer-details',
   templateUrl: './beer-details.component.html',
-  styleUrls: ['./beer-details.component.css']
+  styleUrls: ['./beer-details.component.css'],
+  providers: [BeerService]
 })
 export class BeerDetailsComponent implements OnInit {
 
-  constructor() { }
+  public beerId: string;
+  public isLoading: boolean;
+  public hasError: boolean;
+  public beer: Beer;
 
-  ngOnInit() {
+  constructor(private beerService: BeerService, private activeRoute: ActivatedRoute) {
   }
 
+  ngOnInit() {
+    this.loadBeer();
+  }
+
+  loadBeer(): void {
+    this.isLoading = true;
+    this.hasError = false;
+    this.activeRoute.params.subscribe(params => {
+      this.beerId = params['id'];
+      this.beerService.getBeerById(this.beerId).subscribe(res => {
+        this.beer = res.data;
+        console.log(this.beer);
+        this.isLoading = false;
+      })
+    }, error => {
+      console.log(error);
+      this.hasError = true;
+      this.isLoading = false;
+    });
+  }
 }
