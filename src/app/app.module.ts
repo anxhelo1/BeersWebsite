b63910982da6f91597ada4b5be4b6577d27c63c7
Anxhelo <anxhelosako@gmail.com>
2018-04-15 11:49:13 +0200

    beer list view changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BeerListComponent } from './components/beer-list/beer-list.component';
-import { BeerDetailsComponent } from './components/beer-details/beer-details.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BeerListComponent} from './components/beer-list/beer-list.component';
+import {BeerDetailsComponent} from './components/beer-details/beer-details.component';
+import {NavbarComponent} from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,12 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/components/beer-list/beer-list.component.ts
+++ src/app/components/beer-list/beer-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BeerService} from '../../services/beer.service';
+import {Beer} from '../../models/Beer';
 
 @Component({
   selector: 'app-beer-list',
   templateUrl: './beer-list.component.html',
-  styleUrls: ['./beer-list.component.css']
+  styleUrls: ['./beer-list.component.css'],
+  providers: [BeerService]
 })
 export class BeerListComponent implements OnInit {
 
-  constructor() { }
+  public beers: Beer[];
+  public isLoading: boolean;
+
+  constructor(private beerService: BeerService) {
+    this.beers = [];
+  }
 
   ngOnInit() {
+    this.isLoading = true;
+    this.beerService.getBeers().subscribe(res => {
+      this.beers = res.data.filter(b => b.nameDisplay !== undefined || b.description !== undefined);
+      this.isLoading = false;
+    });
+
   }
 
 }
